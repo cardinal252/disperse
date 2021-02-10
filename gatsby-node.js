a89a20bda9f69ba7ba008c0673de5853e60a4142
JakeMann83 <55398370+JakeMann83@users.noreply.github.com>
2021-02-10 10:28:27 +0000

    pulling page title from contenttype

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,9 +2,16 @@ const path = require('path')
 
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
-
+    
     const CMSpage = await graphql(`
         query {
+            allContentfulContentType {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
             allContentfulEntry {
                 edges {
                   node {
@@ -19,11 +26,25 @@ module.exports.createPages = async ({ graphql, actions }) => {
               }   
         }  
     `)
+
+    const newpagetemplate = `{page.data.allContentfulContentType.edges.node.name}`;
+    
     const page = CMSpage
 
     page.data.allContentfulEntry.edges.forEach((edge) => {
+       
+      var template = { newpagetemplate };
+
+        switch(template) {
+          case 'TCarticle':
+          template = './src/templates/TSarticle.js';
+          break;
+          case 'Template2':
+          template = './src/templates/LinesCalls.js';
+          }
+
         createPage({
-            component: path.resolve('./src/templates/TSarticle.js'),
+            component: path.resolve(template),
             path: `/${edge.node.slug}`,
             context: {
                 slug: edge.node.slug    
